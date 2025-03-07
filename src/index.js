308afd6a61c94254e55bd8dab0dfb136d8ba528e
USER\asus1 <dodi.arahmat@gmail.com>
2025-03-07 14:17:43 +0700

    create index_useref_render

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  const [calculation, setCalculation] = useState(0);
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
 
   useEffect(() => {
-    setCalculation(() => count * 2);
-  }, [count]); // <- add the count variable here
+    count.current = count.current + 1;
+  });
 
   return (
     <>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount((c) => c +1 )}>+</button>
-      <p>Calculation: {calculation}</p>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
     </>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Counter />);
+root.render(<App />);
